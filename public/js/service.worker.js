b5f532c40827038757bfce2a209537d2a08cce49
AimeeJay <ajesso@post.com>
2021-06-29 23:31:53 -0400

    service worker code added, followed module 18 and 19 lesson 4

---
+++ public/js/service.worker.js
@@ -0,0 +1,74 @@
+const FILES_TO_CACHE = [
+    //adding everything public
+    '/index.html',
+    '/js/index.js',
+    '/js/idb.js',
+    '/css/styles.css',
+    '/manifest.json',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png',
+    '/'
+];
+
+const APP_PREFIX = 'BudgetTracker-';
+
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+//from modlule 19 lesson 4
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+});
+
+//activate
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+            // delete all previous cache versions first 
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            })
+            );
+        })
+    )
+});
+
+// retrieving info lesson 4
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+
+                // if present respond
+                console.log('responding with cache : ' + e.request.url)
+                //show cache response
+                return request
+            } else {
+
+                // if none log result
+                console.log('file is not cached, fetching : ' + e.request.url)
+
+                return fetch(e.request)
+            }
+        })
+    )
+});
